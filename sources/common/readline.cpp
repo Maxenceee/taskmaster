6355d7d095b36ce34ae58778e526541a81742da5
Maxence Gama <72473014+Maxenceee@users.noreply.github.com>
2025-03-25 11:37:01 +0100

    prevent path completion

--- sources/common/readline.cpp
+++ sources/common/readline.cpp
@@ -6,14 +6,14 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 20:07:36 by mgama             #+#    #+#             */
-/*   Updated: 2025/03/19 17:15:11 by mgama            ###   ########.fr       */
+/*   Updated: 2025/03/25 11:24:39 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "readline.hpp"
 #include "utils/utils.hpp"
 
-int tty_fd = open("/dev/pts/9", O_RDWR);
+int tty_fd = open("/dev/pts/10", O_RDWR);
 
 enum tm_rl_ev {
 	TM_RL_NEW_LINE = 0,
