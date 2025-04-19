8e5a4b2244153c6b9b37a3fbbc29fe819420d996
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2025-04-19 19:16:49 +0200

    replicas

--- sources/daemon/process/Process.cpp
+++ sources/daemon/process/Process.cpp
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 18:45:28 by mgama             #+#    #+#             */
-/*   Updated: 2025/04/19 13:17:02 by mgama            ###   ########.fr       */
+/*   Updated: 2025/04/19 19:02:46 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,22 @@
 #include "utils/utils.hpp"
 #include "logger/Logger.hpp"
 
-Process::Process(char* const* exec, char* const* envp, pid_t ppid, int std_in_fd, int std_out_fd, int std_err_fd, tm_process_config &config)
+Process::Process(char* const* exec, char* const* envp, const char* program_name, tm_process_config &config, pid_t ppid, pid_t pgid): _program_name(program_name)
 {
-	this->pid = -1;
+	this->pid = 0;
 	this->ppid = ppid;
-	this->pgid = 0;
+	this->pgid = pgid;
 	this->_wpstatus = 0;
 	this->_signal = 0;
 	this->_exit_code = 0;
 	this->_state = TM_P_STOPPED;
 	this->_retries = 0;
 
-	this->std_in_fd = std_in_fd;
-	this->std_out_fd = std_out_fd;
-	this->std_err_fd = std_err_fd;
+	this->std_in_fd = -1;
+	this->std_out_fd = -1;
+	this->std_err_fd = -1;
+
+	this->_process_group_id = 0; // 0 means it is the leader of the process group
 
 	this->config = config;
 
@@ -38,11 +40,35 @@ Process::Process(char* const* exec, char* const* envp, pid_t ppid, int std_in_fd
 
 Process::~Process(void)
 {
-	if (this->_state == TM_P_RUNNING && this->pid != -1)
+	if (this->_state == TM_P_RUNNING && this->pid != 0)
 	{
 		std::cout << "Killing child " << this->pid << std::endl;
 		(void)this->stop();
 	}
+}
+
+void
+Process::setStdInFd(int std_in_fd)
+{
+	this->std_in_fd = std_in_fd;
+}
+
+void
+Process::setStdOutFd(int std_out_fd)
+{
+	this->std_out_fd = std_out_fd;
+}
+
+void
+Process::setStdErrFd(int std_err_fd)
+{
+	this->std_err_fd = std_err_fd;
+}
+
+void
+Process::setGroupId(int id)
+{
+	this->_process_group_id = id;
 }
 
 int
@@ -54,7 +80,7 @@ Process::_spawn(void)
 		return (TM_FAILURE);
 	}
 
-	this->pid = -1;
+	this->pid = 0;
 	this->_signal = 0;
 	this->_exit_code = 0;
 	this->_state = TM_P_STARTING;
@@ -67,7 +93,7 @@ Process::_spawn(void)
 		return (TM_FAILURE);
 	}
 
-	if ((this->pid = spawn_child(this->exec, this->envp, this->std_in_fd, this->std_out_fd, this->std_err_fd, this->pgid)) == -1)
+	if ((this->pid = spawn_child(this->exec, this->envp, this->std_in_fd, this->std_out_fd, this->std_err_fd, this->pgid)) == 0)
 	{
 		Logger::perror("could not spawn child");
 		this->_state = TM_P_FATAL;
@@ -77,15 +103,6 @@ Process::_spawn(void)
 	std::cout << "Child spawned with pid " << this->pid << std::endl;
 
 	return (TM_SUCCESS);
-}
-
-void
-Process::setGroupId(pid_t pgid)
-{
-	if (pgid >= 0)
-	{
-		this->pgid = pgid;
-	}
 }
 
 int
@@ -119,7 +136,7 @@ Process::restart(void)
 int
 Process::stop(void)
 {
-	if (this->_signal > 0 || this->_state == TM_P_STOPPING || this->_state == TM_P_EXITED || this->_state == TM_P_FATAL || this->pid == -1)
+	if (this->_signal > 0 || this->_state == TM_P_STOPPING || this->_state == TM_P_EXITED || this->_state == TM_P_FATAL || this->pid == 0)
 	{
 		std::cout << "Process already stopping or stopped" << std::endl;
 		return (TM_SUCCESS);
@@ -134,7 +151,7 @@ Process::stop(void)
 int
 Process::signal(int sig)
 {
-	if (this->_state != TM_P_RUNNING || this->pid == -1)
+	if (this->_state != TM_P_RUNNING || this->pid == 0)
 	{
 		std::cout << "Process already stopping or stopped" << std::endl;
 		return (TM_SUCCESS);
@@ -146,7 +163,7 @@ Process::signal(int sig)
 int
 Process::kill(void)
 {
-	if (this->_signal > 0 || this->_state == TM_P_EXITED || this->_state == TM_P_FATAL || this->pid == -1)
+	if (this->_signal > 0 || this->_state == TM_P_EXITED || this->_state == TM_P_FATAL || this->pid == 0)
 	{
 		return (TM_SUCCESS);
 	}
@@ -159,7 +176,7 @@ Process::kill(void)
 int
 Process::_wait(void)
 {
-	if (this->pid == -1)
+	if (this->pid == 0)
 	{
 		return (TM_FAILURE);
 	}
