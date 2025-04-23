176dd5d1f7f88dfdf467b3638c6cce4a1dff3700
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2025-04-23 22:19:29 +0200

    file logging done

--- sources/common/logger/Logger.cpp
+++ sources/common/logger/Logger.cpp
@@ -6,16 +6,17 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 20:48:56 by mgama             #+#    #+#             */
-/*   Updated: 2025/04/23 20:39:58 by mgama            ###   ########.fr       */
+/*   Updated: 2025/04/23 22:19:06 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Logger.hpp"
 
 bool Logger::_debug = false;
-bool Logger::_initiated = false;
 pthread_mutex_t Logger::_loggerMutex;
-Logger::LoggerFileStream Logger::filestream;
+bool Logger::_initiated = false;
+
+Logger::LoggerFileStream Logger::cout;
 std::ofstream Logger::_logFile;
 bool Logger::_file_logging = false;
 bool Logger::_rotation_logging = false;
@@ -49,23 +50,32 @@ operator<<(std::ostream& os, const Logger::LoggerDisplayReset&)
 inline std::ostream&
 operator<<(std::ostream& os, const Logger::LoggerDisplayDate&)
 {
-	struct tm *tm;
-	time_t rawtime;
-	char buf[32];
+	auto now = std::chrono::system_clock::now();
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
 
-	(void)time(&rawtime);
-	tm = localtime(&rawtime);
-	int ret = strftime(buf, 32, "%T", tm);
-	buf[ret] = '\0';
-	os << Logger::Color(CYAN) << "[" << buf << "] " << Logger::DisplayReset;
+	std::tm tm = *std::localtime(&time);
+
+	os << Logger::Color(CYAN) << "[" << std::put_time(&tm, "%H:%M:%S") << "] " << Logger::DisplayReset;
+	return (os);
+}
+
+inline std::ostream&
+operator<<(std::ostream& os, const Logger::LoggerDisplayDay&)
+{
+	auto now = std::chrono::system_clock::now();
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+	std::tm tm = *std::localtime(&time);
+
+	os << Logger::Color(CYAN) << "[" << std::put_time(&tm, "%Y-%m-%d %H:%M:%S") << "] " << Logger::DisplayReset;
 	return (os);
 }
 
 inline Logger::LoggerFileStream&
 operator<<(Logger::LoggerFileStream& lfs, std::ostream& (*manip)(std::ostream&))
 {
-	(*manip)(lfs);
-	lfs.flushToFile();
+	manip(static_cast<std::ostream&>(lfs));
+	lfs.flush();
 	return lfs;
 }
 
@@ -81,6 +91,7 @@ Logger::init(const char *action)
 
 	std::cout << Logger::DisplayDate << TM_PREFIX << action << ": New logger session" << std::endl;
 	std::cerr << Logger::DisplayDate << TM_PREFIX << action << ": New logger session" << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << TM_PREFIX << action << ": New logger session" << std::endl;
 
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_INFO, "%s: New logger session", action);
@@ -142,7 +153,7 @@ Logger::printHeader(bool tty_fallback)
 }
 
 void
-Logger::cout(const char *msg)
+Logger::syn(const char *msg)
 {
 	(void)Logger::aquireMutex();
 
@@ -152,9 +163,9 @@ Logger::cout(const char *msg)
 }
 
 void
-Logger::cout(const std::string &msg)
+Logger::syn(const std::string &msg)
 {
-	Logger::cout(msg.c_str());
+	Logger::syn(msg.c_str());
 }
 
 void
@@ -163,7 +174,8 @@ Logger::print(const char *msg, const char *color)
 	(void)Logger::aquireMutex();
 
 	std::cout << Logger::DisplayDate << Logger::Color(color) << msg << Logger::DisplayReset << std::endl;
-	Logger::filestream << Logger::DisplayDate << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << msg << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_INFO, "%s", msg);
 #endif
@@ -183,7 +195,8 @@ Logger::info(const char *msg)
 	(void)Logger::aquireMutex();
 
 	std::cout << Logger::DisplayDate << Logger::Color(YELLOW) << TM_PREFIX << msg << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Info: " << msg << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_INFO, "%s", msg);
 #endif
@@ -203,7 +216,8 @@ Logger::warning(const char *msg)
 	(void)Logger::aquireMutex();
 
 	std::cerr << Logger::DisplayDate << Logger::Color(B_ORANGE) << TM_PREFIX << msg << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Warning: " << msg << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_WARNING, "%s", msg);
 #endif
@@ -223,7 +237,8 @@ Logger::error(const char *msg)
 	(void)Logger::aquireMutex();
 
 	std::cerr << Logger::DisplayDate << Logger::Color(B_RED) << TM_PREFIX << msg << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Error: " << msg << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_ERR, "%s", msg);
 #endif
@@ -243,7 +258,8 @@ Logger::perror(const char *msg)
 	(void)Logger::aquireMutex();
 
 	std::cerr << Logger::DisplayDate << Logger::Color(B_RED) << TM_PREFIX << msg << ": " << strerror(errno) << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Error: " << msg << ": " << strerror(errno) << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_ERR, "%s: %s", msg, strerror(errno));
 #endif
@@ -263,7 +279,8 @@ Logger::pherror(const char *msg)
 	(void)Logger::aquireMutex();
 
 	std::cerr << Logger::DisplayDate << Logger::Color(B_RED) << TM_PREFIX << msg << ": " << hstrerror(h_errno) << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Error: " << msg << ": " << hstrerror(h_errno) << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_ERR, "%s: %s", msg, hstrerror(h_errno));
 #endif
@@ -286,7 +303,8 @@ Logger::debug(const char *msg, const char *color)
 	(void)Logger::aquireMutex();
 
 	std::cout << Logger::DisplayDate << Logger::Color(color) << msg << Logger::DisplayReset << std::endl;
-	Logger::filestream << msg << std::endl;
+	Logger::cout << Logger::DisplayDay << TM_PROJECTD << "[" << getpid() << "] - " << "Debug: " << msg << std::endl;
+
 #ifndef TM_DISABLE_SYSLOG
 	syslog(LOG_DEBUG, "%s", msg);
 #endif
@@ -313,27 +331,24 @@ Logger::isDebug(void)
 }
 
 void
-Logger::enableFileLogging(void)
+Logger::enableFileLogging(const std::string& fname)
 {
 	Logger::_file_logging = true;
 	if (Logger::_logFile.is_open())
 	{
 		Logger::_logFile.close();
 	}
-	Logger::_logFile.open(TM_MAIN_LOG_DIR TM_PROJECT ".log", std::ios::out | std::ios::app);
+	Logger::_logFile.open(fname, std::ios::out | std::ios::app);
+}
+
+void
+Logger::enableFileLogging(void)
+{
+	Logger::enableFileLogging(TM_MAIN_LOG_DIR TM_PROJECTD ".log");
 }
 
 void
 Logger::enableRotationLogging(void)
 {
 	Logger::_rotation_logging = true;
-}
-
-Logger::LoggerFileStream::LoggerFileStream(): std::ostream(_buffer.rdbuf())
-{
-}
-
-Logger::LoggerFileStream::~LoggerFileStream()
-{
-	flushToFile();
 }
